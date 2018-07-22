7370c701e385de0348a425c794944d1f8132b8d3
Rion Dooley <dooley@tacc.utexas.edu>
2018-07-21 22:59:07 -0500

    * Merging updated, condensed singularitycheck.sh file from Steve. * Removing all singularity check info from bashrc.

--- training-sandbox/singularitycheck.sh
+++ training-sandbox/singularitycheck.sh
@@ -1,4 +1,28 @@
 #!/bin/sh
+env > /tmp/env.txt
+# Put the full path to a singularity image in the file $HOME/sing.txt.
+if [ -r $HOME/work/sing.txt ]
+then
+    IMAGE=$(cat $HOME/work/sing.txt)
+fi
+if [ "$IMAGE" != "" ]
+then
+    if [ -r "$IMAGE" ]
+    then
+        # If the SINGULARITY_CONTAINER variable is set,
+        # then we are already in the container
+        if [ "$SINGULARITY_CONTAINER" = "" ]
+        then
+            # Switch to running inside singularity
+            if [ "$SSH_ORIGINAL_COMMAND" = "" ]
+            then
+                exec singularity exec $IMAGE bash --login
+            else
+                exec singularity exec $IMAGE bash --login -c "$SSH_ORIGINAL_COMMAND"
+            fi
+        fi
+    fi
+fi
 
 if [ -n "$SINGULARITY_CONTAINER" ];
 then
